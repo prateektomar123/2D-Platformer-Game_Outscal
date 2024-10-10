9cc918a6be2120833a04bd52203ebaa5e35a6a35
Prateek <prateektomar005@gmail.com>
2024-10-10 22:39:09 +0530

    Added the  enemy and its patrolling behaviour

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -78,12 +78,11 @@ public class PlayerController : MonoBehaviour
                 SceneManager.LoadScene(0);
             }
         }
-        else if (collision.gameObject.CompareTag("key"))
+        else if (collision.gameObject.CompareTag("Enemy"))
         {
-            scoreController.incScore();
-            collision.gameObject.GetComponent<Animator>().SetBool("destroyed",true);
-            Destroy(collision.gameObject,2.5f);
+            Debug.Log("Player Died");
         }
+        
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
@@ -91,6 +90,13 @@ public class PlayerController : MonoBehaviour
         {
             isGrounded = false;
             
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("key")){
+            scoreController.incScore();
+            other.GetComponent<Animator>().SetBool("destroyed",true);
+            Destroy(other,2.5f);
         }
     }
     void crouch(bool isCrouching)
